0e484b7091234b2d95599dba059eae7fdb24f919
nirgluzman <nir.gluzman@gmail.com>
2024-07-07 09:13:52 +0200

    upload an empty Helm index.yaml file to Helm repository in S3

--- remote-state-config/s3-helm-repo.tf
+++ remote-state-config/s3-helm-repo.tf
@@ -1,3 +1,4 @@
+# Helm chart repository in Amazon S3
 resource "aws_s3_bucket" "helm-charts-repo" {
   bucket = "nioyatech-helm-repo"
 
@@ -6,4 +7,12 @@ resource "aws_s3_bucket" "helm-charts-repo" {
   tags = {
     Name = "Helm Charts Repository"
   }
+}
+
+# upload an empty Helm index.yaml file
+resource "aws_s3_object" "chart-index" {
+  bucket = "${aws_s3_bucket.helm-charts-repo.bucket}"
+  key    = "charts/index.yaml" # name of the object once it is in the bucket
+  source = "./index.yaml" # path to a file that will be read and uploaded
+
 }
